9e438dda76b3586d3b7a7215ef9ac647100fe538
killshotroxs <krishnal@hotmail.com>
2023-08-01 03:22:30 +0530

    Basic working version

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-alert("Active");
+function getRandomDogImage() {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(response => response.json())
+      .then(data => {
+        const dogImageUrl = data.message;
+        const dogImageDiv = document.getElementById("dogimage");
+        dogImageDiv.innerHTML = `<img src="${dogImageUrl}" alt="Random Dog Image">`;
+      })
+      .catch(error => {
+        console.error("Error fetching dog image:", error);
+      });
+  }
